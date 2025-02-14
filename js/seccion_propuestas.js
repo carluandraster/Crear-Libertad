9913da890a5063851e8b294856b3ebcb87741e87
carluandraster <andres.efstratiadis@gmail.com>
2025-02-14 17:57:46 -0300

    Actualizacion

---
+++ js/seccion_propuestas.js
@@ -0,0 +1,14 @@
+const seccionProp = document.getElementById("four")
+
+class Articulo {
+    constructor(imagen,titulo,descripcion,link) {
+        this.imagen = imagen;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.link = link;
+    }
+
+    mostrarse() {
+        
+    }
+}
