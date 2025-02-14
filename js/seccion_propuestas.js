ff64b3929a22e0e112097c765d5123be4b52d19a
carluandraster <andres.efstratiadis@gmail.com>
2025-02-14 19:47:27 -0300

    Update seccion_propuestas.js

--- js/seccion_propuestas.js
+++ js/seccion_propuestas.js
@@ -1,4 +1,4 @@
-const seccionProp = document.getElementById("four")
+const seccionProp = document.querySelector("#four .features")
 
 class Articulo {
     constructor(imagen,titulo,descripcion,link) {
@@ -9,6 +9,10 @@ class Articulo {
     }
 
     mostrarse() {
-        
+        cuadro = document.createElement("article");
+        cuadro.innerHTML = `<a href="${this.link}" class="image"><img src="images/pic04.jpg" alt="" /></a>
+							<h3 class="major">Fotocopiadora</h3>
+							<p>Descubrí nuestras propuestas para ofrecerle al alumnado fotocopias de mejor calidad a un mejor precio.</p>
+							<a href="${this.link}" class="special">Leer más</a>`;
     }
 }
