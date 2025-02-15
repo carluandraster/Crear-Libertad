a07f36255bcc14f4d1e4129efcb97e78753170ba
carluandraster <andres.efstratiadis@gmail.com>
2025-02-14 23:27:00 -0300

    A

--- js/seccion_propuestas.js
+++ js/seccion_propuestas.js
@@ -10,9 +10,12 @@ class Articulo {
 
     mostrarse() {
         cuadro = document.createElement("article");
-        cuadro.innerHTML = `<a href="${this.link}" class="image"><img src="images/pic04.jpg" alt="" /></a>
-							<h3 class="major">Fotocopiadora</h3>
-							<p>Descubrí nuestras propuestas para ofrecerle al alumnado fotocopias de mejor calidad a un mejor precio.</p>
+        cuadro.innerHTML = `<a href="${this.link}" class="image"><img src="${this.imagen}" alt="" /></a>
+							<h3 class="major">${this.titulo}</h3>
+							<p>${this.descripcion}</p>
 							<a href="${this.link}" class="special">Leer más</a>`;
+        seccionProp.appendChild(cuadro);
     }
 }
+
+Articulo("")
